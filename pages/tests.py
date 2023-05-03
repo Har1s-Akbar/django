2090f6ea3f15e9ea1656577b2da91e453ca03f2b
Har1s-Akbar <stanker801@gmail.com>
2023-05-03 01:34:10 -0700

    added some more tests for pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,12 +1,21 @@
 from django.test import TestCase, SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomeView
 
 class HomeTest(SimpleTestCase):
-    def Setup(self):
+    def setUp(self):
         url = reverse('home')
         self.response = self.client.get(url)
+        
     def test_homepage(self):
         self.assertEqual(self.response.status_code, 200)
     def test_template(self):
         self.assertTemplateUsed(self.response, 'home.html')
-    
+    def testCorrectHtml(self):
+        self.assertContains(self.response, 'home')
+    def testWrongHtml(self):
+        self.assertNotContains(self.response, 'Hi, I should not be on the page')
+
+    def testView(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, HomeView.as_view().__name__)
