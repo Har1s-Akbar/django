63ea0cda1b2591ca2cb05fdcab334fdcc7e661f7
Har1s-Akbar <stanker801@gmail.com>
2023-05-09 03:46:11 -0700

    some minor changes in settings including MEDIA_ROOT & URL

--- book_project/settings.py
+++ book_project/settings.py
@@ -158,6 +158,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = '/media/'
+MEDIA_URL = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 #tells Django how to look for static file directories
